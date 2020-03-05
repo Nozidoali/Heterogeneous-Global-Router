48719d46ec9c56ba8a1ed5b250a7006994b206e6
Nozidoali <whynull@sjtu.edu.cn>
2020-03-05 01:18:11 -0600

    version 2.0

--- testing/evaluate.sh
+++ testing/evaluate.sh
@@ -1,10 +1,4 @@
 cd ..
-cmake .
-make
-./ROUTER.exe benchmarks/adaptec1.gr benchmarks/adaptec1.out
-./ROUTER.exe benchmarks/adaptec2.gr benchmarks/adaptec2.out
-./ROUTER.exe benchmarks/adaptec3.gr benchmarks/adaptec3.out
-
 ./556_eval.exe benchmarks/adaptec1.gr benchmarks/adaptec1.out 1
 ./556_eval.exe benchmarks/adaptec2.gr benchmarks/adaptec2.out 1
 ./556_eval.exe benchmarks/adaptec3.gr benchmarks/adaptec3.out 1
---
+++ testing/route.sh
@@ -0,0 +1,8 @@
+cd ..
+cmake .
+make
+time ./ROUTER.exe benchmarks/adaptec1.gr benchmarks/adaptec1.out
+sleep 1
+time ./ROUTER.exe benchmarks/adaptec2.gr benchmarks/adaptec2.out
+sleep 1
+time ./ROUTER.exe benchmarks/adaptec3.gr benchmarks/adaptec3.out
